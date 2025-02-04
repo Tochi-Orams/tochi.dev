1c5ca949a59b4663121fb4b8c7dcabef450b1c9d
Tochi Oramasionwu <tochi.orams@gmail.com>
2025-02-04 17:50:59 -0500

    tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import Providers from "./Context/Providers";
 import MusicPlayer from "./Components/MusicPlayer";
 import Nav from "./Components/Nav";
 import MobileNav from "./Components/MobileNav";
+import Script from "next/script";
 
 // config.autoAddCss = false;
 
@@ -32,10 +33,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="relative">
+      <Script
+        id="Google Tag"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','G-09GLEN08ZQ')`,
+        }}
+      />
       <Providers>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <Script
+            async
+            src="https://www.googletagmanager.com/ns.html?id=G-09GLEN08ZQ"
+            id="google-tag"
+          ></Script>
+          <Script id="tag-code">
+            {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-09GLEN08ZQ');`}
+          </Script>
           <Nav />
           <MobileNav />
           <MusicPlayer />
